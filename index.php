37dbcac4fc2dff2c5cc9c229bb94a2e8a6d419c8
riyansobirin-indonesia <riyansobirin2@gmail.com>
2020-12-31 11:32:01 +0700

    Menambahkan List

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
   <h1>Hello World !</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ut quisquam aperiam iure, ipsam repudiandae, accusantium ullam odit tempore impedit veniam. Quis soluta animi facere porro perferendis hic laudantium vitae!</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 
 </html>
